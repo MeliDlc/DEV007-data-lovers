cf0ab2a5a7912b93b2fdf5226d71ce1c2f3da84a
MeliDlc <melissasheyladelacruz@gmail.com>
2023-05-22 10:25:26 -0500

    Updates

---
+++ main.js
@@ -0,0 +1,24 @@
+import data from './data/pokemon/pokemon.js';
+  console.log(data)
+  
+
+
+
+//const dataButton = document.getElementById('dataButton');
+
+//const jsonData = document.getElementById('jsonData')
+
+/*const callJSON = () => {
+    fetch('./data/pokemon/pokemon.json')
+    .then(res => res.json())
+    .then(data => {
+        jsonData.innerText = JSON.stringify(data);
+    })
+    .catch(e => console.error (new Error(e)));
+}
+
+dataButton.addEventListener('click', callJSON);*/
+
+
+
+
